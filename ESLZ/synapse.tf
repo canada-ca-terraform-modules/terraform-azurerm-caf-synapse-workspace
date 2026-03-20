1d1decdc467f99c7a6847c59414360f3d3648d6f
Bernard Maltais <bernard.maltais@ssc-spc.gc.ca>
2026-03-20 09:50:18 -0400

    chore: bump ESLZ module ref to v1.1.0

--- ESLZ/synapse.tf
+++ ESLZ/synapse.tf
@@ -3,7 +3,7 @@ variable "synapse" {
 }
 
 module "synapse-workspace" {
-  source   = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-synapse-workspace?ref=v1.0.0"
+  source   = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-synapse-workspace?ref=v1.1.0"
   for_each = var.synapse
 
   userDefinedString = each.key
